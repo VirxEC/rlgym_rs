72401240931a07dd79cb7098e6b4c8186dd8797f
Virx <virx@virxcase.dev>
2025-03-29 00:21:23 +0000

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -170,13 +170,16 @@ where
         if let Some(renderer) = &mut self.renderer {
             renderer.send_state(&raw_state).unwrap();
         }
-        
+
         let state = Rc::new(raw_state.to_glam());
 
         // assert that the order of cars in state is the same as in mapped_actions
-        mapped_actions.into_iter().zip(&state.cars).for_each(|((car_id, _), car)| {
-            debug_assert_eq!(car.id, car_id);
-        });
+        mapped_actions
+            .into_iter()
+            .zip(&state.cars)
+            .for_each(|((car_id, _), car)| {
+                debug_assert_eq!(car.id, car_id);
+            });
 
         self.shared_info_provider
             .apply(&state, &mut self.shared_info);
